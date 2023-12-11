223513841ea3b3235dc6752789caf29cfbb225fc
Christoph Stockinger <christoph@morethings.digital>
2023-12-12 00:32:41 +0100

    feat(routes): add index route

--- routes/cp.php
+++ routes/cp.php
@@ -1,2 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Morethingsdigital\StatamicNextjs\Http\Controllers\IndexController;
+
+Route::prefix('nextjs')->name('nextjs.')->group(function () {
+  Route::get('/', [IndexController::class, 'index'])->name('index');
+});
