e47db1e6cd3a0b20e323ca6860f77aefd27ed490
Christoph Stockinger <christoph@morethings.digital>
2024-10-11 15:12:59 +0200

    feat: update routes

--- routes/cp.php
+++ routes/cp.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Morethingsdigital\StatamicNextjs\Http\Controllers\IndexController;
+use Morethingsdigital\StatamicNextjs\Http\Controllers\InvalidateController;
 
 Route::prefix('nextjs')->name('nextjs.')->group(function () {
-  Route::get('/', [IndexController::class, 'index'])->name('index');
+    Route::get('/', [IndexController::class, 'index'])->name('index');
+    Route::post('/invalidate', [InvalidateController::class, 'invalidate'])->name('invalidate');
 });
