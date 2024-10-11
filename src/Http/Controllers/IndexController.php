3e3487aa51240721103c771bfd39fdc4a463641a
Christoph Stockinger <christoph@morethings.digital>
2024-10-11 15:12:42 +0200

    feat: add index controller

--- src/Http/Controllers/IndexController.php
+++ src/Http/Controllers/IndexController.php
@@ -2,23 +2,51 @@
 
 namespace Morethingsdigital\StatamicNextjs\Http\Controllers;
 
+use Morethingsdigital\StatamicNextjs\Services\SiteService;
+use Morethingsdigital\StatamicNextjs\Services\TagService;
+use Statamic\Facades\Collection;
+use Statamic\Facades\GlobalSet;
+use Statamic\Facades\Nav;
+use Statamic\Facades\Taxonomy;
 use Statamic\Facades\User;
 use Statamic\Http\Controllers\CP\CpController;
 
 class IndexController extends CpController
 {
 
-    public function __construct()
-    {
-    }
+    public function __construct(
+        private readonly SiteService $siteService,
+        private readonly TagService $tagService
+    ) {}
 
     public function index()
     {
         abort_unless(User::current()->can('view nextjs'), 403);
 
-        // $personioLogs = User::current()->can('view logs personio') ? $this->logService->find() : [];
-
         $title = 'Statamic x Next.js';
+
+        $selectedSite = $this->siteService->currenteSite();
+
+        $collectionTags = $this->tagService->findCollectionTags(selectedSite: $selectedSite->handle());
+
+        $collections = collect($collectionTags)->map(function ($handle) {
+            return Collection::findByHandle($handle);
+        }) ?? [];
+
+        $globalTags =  $this->tagService->findGlobalsTags(selectedSite: $selectedSite->handle());
+
+        $globals = $globalTags ? GlobalSet::all() : [];
+
+        $navigationTags = $this->tagService->findNavigationTags(selectedSite: $selectedSite->handle());
+
+        $navigations = $navigationTags ? Nav::all() : [];
+
+        $taxonomyTags = $this->tagService->findTaxonomyTags(selectedSite: $selectedSite->handle());
+
+        $taxonomies = $taxonomyTags ? Taxonomy::all() : [];
+        // dd($this->tagService->all(selectedSite: $selectedSite->handle()));
+
+
 
         // $routes = [
         //     'import' => [
@@ -29,6 +57,6 @@ class IndexController extends CpController
         //     ]
         // ];
 
-        return view('nextjs::index', compact('title'));
+        return view('nextjs::index', compact('title', 'collections', 'globals', 'navigations', 'taxonomies'));
     }
 }
