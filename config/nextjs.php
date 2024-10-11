84916f4d2d1f6cd7bad21f8cf3187e2dbd6c389b
Christoph Stockinger <christoph@morethings.digital>
2024-10-11 15:12:26 +0200

    feat: update config

--- config/nextjs.php
+++ config/nextjs.php
@@ -1,39 +1,38 @@
 <?php
 
 return [
-  'enabled' => env('STATAMIC_NEXTJS_ENABLED', true),
-  'revalidation_urls' => env('STATAMIC_NEXTJS_REVALIDATION_URLS', []),
-  'revalidation_secret' => env('STATAMIC_NEXTJS_REVALIDATION_SECRET', 'secret'),
-  'revalidation_type' => env('STATAMOC_NEXTJS_REVALIDATION_TYPE', 'tag'), // tag or path
-  'custom_tag_revalidation' => [
-    'collections' => [
-      'page' => 'pages'
-      // '<collection>' => '<tag>'
+    'enabled' => env('STATAMIC_NEXTJS_ENABLED', true),
+    'revalidation_urls' => [
+        'default' => 'http://localhost:3000/api/revalidation',
+        // 'site-handle-two' => env('FRONTEND_URL_K16') . "/api/revalidation",
+        // ... add more site handle revalidation urls here
     ],
-    'globals' => 'globals',
-    'navigation' => 'navigation',
-    'taxonomies' => null
-  ]
+    'revalidation_secret' => env('STATAMIC_NEXTJS_REVALIDATION_SECRET', 'secret'),
+    'revalidation_type' => env('STATAMOC_NEXTJS_REVALIDATION_TYPE', 'tag'), // tag or path
+    'custom_tag_revalidation' => [
+        'default' => [
+            'collections' => [
+                'pages' => ['pages'],
+                // 'news' => ['news'],
+                // ... add more collections here
+            ],
+            'globals' => ['globals'],
+            'navigation' => ['navigation'],
+            'taxonomies' => [
+                // 'categories' => ['categories'],
+                // ... add more taxonomies here
+            ],
+            'assets' => ['assets'],
+        ],
+        // 'site-handle-two' => [
+        //     'collections' => [
+        //         'pages' => ['pages'],
+        //     ],
+        //     'globals' => ['globals'],
+        //     'navigation' => ['navigation'],
+        //     'taxonomies' => [],
+        //     'assets' => ['assets'],
+        // ],
+        // ... add more site handle tag revalidations here
+    ],
 ];
-
-
-// Default Tag Revalidations
-// [
-//   'collection' => [
-//     'pages' => 'pages',
-//     '*' => '*'
-//   ],
-//   'globals' => 'globals',
-//   'navigation' => 'navigation'
-// ]
-
-
-// TBD:
-
-// Default Path Revalidations
-// [
-//   'collection' => [
-//     'pages' => ['/:handle'],
-//     '*' => ['/*/:handle']
-//   ],
-// ]
